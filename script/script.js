f6ab756f1402645a96e5b075f6b3b6adf0da3638
Anton <antonr1@bk.ru>
2020-03-12 18:00:09 +0300

    add first modal

--- script/script.js
+++ script/script.js
@@ -2,17 +2,36 @@ window.addEventListener('DOMContentLoaded', function () {
 	'use strict';
 
 	//модальное окно 1
+	const tooglePopUp = () => {
+		//получаем необходимые элементы
+		const callBtn = document.querySelectorAll('a[class="call-btn"]'),
+			popUpCall = document.querySelector('.popup-call');
 
-	//получаем необходимые элементы
-	const callBtn = document.querySelectorAll('a[class="call-btn"]'),
-		popUpCall = document.querySelector('.popup-call');
 
-	// Вешаем обработчик события. Открываем модальное окно при клике на "Перезвоните мне" в хедере и футере
-	callBtn.forEach((elem) => {
-		elem.addEventListener('click', () => {
-			popUpCall.style.display = 'block';
+		// Вешаем обработчик события. Открываем модальное окно при клике на "Перезвоните мне" в хедере и футере
+		callBtn.forEach((elem) => {
+			elem.addEventListener('click', () => {
+				popUpCall.style.display = 'block';
+			});
 		});
-	});
+
+		//Обработчик события для закрытия модального окна при клике на крестик или на подложку
+		popUpCall.addEventListener('click', (event) => {
+			let target = event.target;
+
+			if (target.classList.contains('popup-close')) {
+				popUpCall.style.display = 'none';
+			} else {
+				target = target.closest('.popup-content');
+
+				if (!target) {
+					popUpCall.style.display = 'none';
+				}
+			}
+		});
+	};
+
+	tooglePopUp();
 
 
 
