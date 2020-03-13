05b9c535cf82716064ee8cf3e809f65b2d654b2e
Anton <antonr1@bk.ru>
2020-03-13 18:39:04 +0300

    add accordion

--- script/script.js
+++ script/script.js
@@ -33,7 +33,8 @@ window.addEventListener('DOMContentLoaded', function () {
 	};
 
 	//Вызываем функцию открытия/закрытия модального окна при нажатии на "Перезвоните мне"
-	tooglePopUp('.popup-call', '.call-btn');
+	tooglePopUp('.popup-call', 'a[class="call-btn"]');
+	// tooglePopUp('.popup-call', '.call-btn');
 
 	//Вызываем функцию открытия/закрытия модального окна при нажатии на "Заказать со скидкой" и "Узнать цену со скидкой"
 	tooglePopUp('.popup-discount', '.discount-btn');
@@ -120,4 +121,46 @@ window.addEventListener('DOMContentLoaded', function () {
 	sendForm('.capture-form');
 	sendForm('.main-form');
 
+	//функция переключения табов
+	const tabs = (id) => {
+		const accordion = document.getElementById(id),
+			panelHeading = accordion.querySelectorAll('.panel-heading'),
+			panelContent = accordion.querySelectorAll('.panel-collapse'),
+			btn = accordion.querySelectorAll('.construct-btn');
+			console.log('btn: ', btn);
+
+
+		//функция для показа/скрытие выбранных табов
+		const toogleTabContent = (index) => {
+			for (let i = 0; i < panelContent.length; i++) {
+				if (index === i) {
+					panelContent[i].classList.add('in');
+				} else {
+					panelContent[i].classList.remove('in');
+				}
+			}
+		};
+
+		accordion.addEventListener('click', (event) => {
+			event.preventDefault();
+			let target = event.target;
+
+			if (target) {
+				panelHeading.forEach((item, i) => {
+					if (item === target.closest('.panel-heading')) {
+						toogleTabContent(i);
+					}
+				});
+				btn.forEach((item, i) => {
+					if (item === target.closest('.construct-btn')) {
+						toogleTabContent(i+1);
+					}
+				});
+			}
+		});
+
+	};
+
+	tabs('accordion');
+	tabs('accordion-two');
 });
